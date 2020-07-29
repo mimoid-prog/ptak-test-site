447a79a95dc677871ded7947290e4a927c10ab03
mimoid-prog <mimoid.prog@gmail.com>
2020-07-29 16:14:14 +0200

    Test

--- api/visitor.js
+++ api/visitor.js
@@ -27,29 +27,29 @@ module.exports = (req, res) => {
   const values = req.body.values
   res.json({ values })
 
-  // QRCode.toDataURL(values.email)
-  //   .then(newQR => {
-  //     const newVisitor = new Visitor({
-  //       company: values.company,
-  //       name: values.name,
-  //       email: values.email,
-  //       phone: values.phone,
-  //       nip: values.nip,
-  //       qr: newQR,
-  //     })
+  QRCode.toDataURL(values.email)
+    .then(newQR => {
+      const newVisitor = new Visitor({
+        company: values.company,
+        name: values.name,
+        email: values.email,
+        phone: values.phone,
+        nip: values.nip,
+        qr: newQR,
+      })
 
-  //     newVisitor
-  //       .save()
-  //       .then(() => {
-  //         res.json({ qr: newQR })
-  //       })
-  //       .catch(err => {
-  //         console.error(err)
-  //         res.status(400).json({ error: "Creating new visitor error" })
-  //       })
-  //   })
-  //   .catch(err => {
-  //     console.error(err)
-  //     res.status(400).json({ error: "Creating QR code error" })
-  //   })
+      newVisitor
+        .save()
+        .then(() => {
+          res.json({ qr: newQR })
+        })
+        .catch(err => {
+          console.error(err)
+          res.status(400).json({ error: "Creating new visitor error" })
+        })
+    })
+    .catch(err => {
+      console.error(err)
+      res.status(400).json({ error: "Creating QR code error" })
+    })
 }
